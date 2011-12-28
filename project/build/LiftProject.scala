d15ead748e39ba36dc2bb7f3672da28becf7fe0e
John Langley <digerat@gmail.com>
2011-12-28 10:22:14 -0500

    Added the sbt Eclipsify plugin. Note, you have to add it both as a   plugin AND extend your lift project with it in order to use it. Also tweaked .gitignore to ignore the .classpath, .project and some   elements of the plugins directory.

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -1,6 +1,7 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
+class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclipsify {
   val liftVersion = property[Version]
 
   // uncomment the following if you want to use the snapshot repo
@@ -20,6 +21,7 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
     "ch.qos.logback" % "logback-classic" % "0.9.26",
     "org.scala-tools.testing" %% "specs" % "1.6.8" % "test",
     "com.h2database" % "h2" % "1.2.147",
-    "org.scala-tools.testing" %% "specs" % "1.6.9" % "test->default" // For specs.org tests
+    "org.scala-tools.testing" %% "specs" % "1.6.9" % "test->default", // For specs.org tests
+	"de.element34" % "sbt-eclipsify" % "0.8.0-SNAPSHOT"
   ) ++ super.libraryDependencies
 }
