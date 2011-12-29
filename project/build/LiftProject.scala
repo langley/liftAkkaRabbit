65f9af8056cbfe807c80d64c1ff779496dcb9f6f
John Langley <digerat@gmail.com>
2011-12-29 11:06:20 -0500

    W00T! This changeset incorporates AMQP for the first time.   Thanks to Timothy Perrett's example code in:   https://github.com/timperrett/lift-in-action

--- project/build/LiftProject.scala
+++ project/build/LiftProject.scala
@@ -17,8 +17,9 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with Eclips
   lazy val JavaNet = "Java.net Maven2 Repository" at "http://download.java.net/maven/2/"
 
   override def libraryDependencies = Set(
-    "net.liftweb" %% "lift-webkit" % liftVersion.value.toString % "compile",
-    "net.liftweb" %% "lift-mapper" % liftVersion.value.toString % "compile",
+    "net.liftweb" %% "lift-webkit"  % liftVersion.value.toString % "compile",
+    "net.liftweb" %% "lift-mapper"  % liftVersion.value.toString % "compile",
+	"net.liftweb" %% "lift-amqp" % liftVersion.value.toString % "compile",
     "org.mortbay.jetty" % "jetty" % "6.1.26" % "test",
     "junit" % "junit" % "4.7" % "test",
     "ch.qos.logback" % "logback-classic" % "0.9.26",
